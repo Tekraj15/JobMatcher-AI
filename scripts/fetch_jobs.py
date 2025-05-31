b057dbaaa7d24e7b3fcdaf5a8a78c9f6046c8c7a
Tek Raj Awasthi <38464224+Tekraj15@users.noreply.github.com>
2025-05-31 18:13:02 +0200

    Deleting this for unified script

--- scripts/fetch_jobs.py
+++
@@ -1,7 +0,0 @@
-from backend.mantiks_job_api import fetch_jobs_pipeline
-
-fetch_jobs_pipeline(
-    job_titles=["Data scientist", "ML engineer", "Data Engineer", "Working Student Data Engineer"],
-    location_ids=[2867714],  # e.g., Munich
-    limit=10
-)
