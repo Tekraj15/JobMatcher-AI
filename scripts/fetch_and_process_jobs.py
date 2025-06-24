a407c56a072b7f7fa503af9083b4b7acc769b1d9
Tek Raj Awasthi <tekrajawasthi@Teks-MacBook-Air-3.local>
2025-06-24 23:47:49 +0200

    Created own Job IDs using hashlib

--- scripts/fetch_and_process_jobs.py
+++ scripts/fetch_and_process_jobs.py
@@ -3,9 +3,10 @@ import json
 import pandas as pd
 from datetime import datetime
 from argparse import ArgumentParser
-from backend.mantiks_job_api import fetch_jobs_pipeline
+#from backend.mantiks_job_api import fetch_jobs_pipeline
 from dotenv import load_dotenv
 from loguru import logger
+import hashlib
 
 load_dotenv()
 
@@ -16,11 +17,16 @@ os.makedirs(DATA_DIR, exist_ok=True)
 def get_timestamp():
     return datetime.now().strftime("%Y-%m-%d_%H-%M")
 
+# Create own job_ids
+def generate_id_from_url(url: str) -> str:
+    return hashlib.md5(url.encode()).hexdigest()
+
 def flatten_jobs_json(json_path, csv_path):
     with open(json_path, "r") as f:
         jobs = json.load(f)
 
     df = pd.json_normalize(jobs)
+    df["job_id"] = df["job_board_url"].apply(generate_id_from_url)
     df.to_csv(csv_path, index=False)
     logger.success(f"Saved flattened CSV to {csv_path}")
     return df
@@ -33,13 +39,14 @@ def convert_csv_to_parquet(csv_path, parquet_path):
 
 def run_pipeline(job_titles, location_ids, limit):
     timestamp = get_timestamp()
-    json_path = os.path.join(DATA_DIR, f"jobs_{timestamp}.json")
+    #json_path = os.path.join(DATA_DIR, f"jobs_{timestamp}.json")
+    json_path = os.path.join(DATA_DIR, "jobs.json")
     csv_path = os.path.join(DATA_DIR, f"jobs_{timestamp}.csv")
     parquet_path = os.path.join(DATA_DIR, f"jobs_{timestamp}.parquet")
 
     logger.info(f"Fetching jobs for: {job_titles}, locations: {location_ids}")
 
-    fetch_jobs_pipeline(job_titles, location_ids, limit=limit, save_path=json_path)
+    #fetch_jobs_pipeline(job_titles, location_ids, limit=limit, save_path=json_path)
     flatten_jobs_json(json_path, csv_path)
     convert_csv_to_parquet(csv_path, parquet_path)
 
